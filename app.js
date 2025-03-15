0326ba732a94c3dcdfd3d9fdcf4ff4912007f85a
Sergio Benedini <shbenedini@gmail.com>
2025-03-15 15:07:10 -0300

    Função que agraga amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// função para agregar amigos
+function adicionarAmigo() {
+    amigo = document.getElementById('amigo').value;
+    if (amigo == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(amigo);
+    }
+    document.getElementById('amigo').value = '';
+}
+
